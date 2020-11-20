3c98ee39c6a4be41e64f9cb8c24f7c4343907118
hotaruwixoss <hotaruwixoss@gmail.com>
2020-11-20 03:36:55 -0400

    Add project files.

---
+++ ejercicio3 practica 6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ejercicio3_practica_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            C objmensaje = new C();
+
+        }
+    }
+
+    class A
+    {
+        public string mensajeA;
+        public A()
+        {
+           mensajeA = "Hola, este es programa que muestra mensajes.";
+        }
+    }
+
+    class B : A
+    {
+        public string mensajeB;
+        public B()
+        {
+          mensajeB = "Jesucristo es el mismo ayer, hoy y por los siglos.";
+        }
+
+    }
+
+    class C : B
+    {
+        public string mensajeC;
+        public C()
+        {
+          mensajeC = "Sonrie ante los retos de la vida. ";
+        }
+    }
+}
